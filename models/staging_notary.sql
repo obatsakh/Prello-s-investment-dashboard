031e0fbde524b37b3d04921822b25c51b8f09e8c
Obay Al Bitar <obatsakh_bit@hotmail.com>
2023-11-28 11:08:25 +0000

    notary cleaned

--- models/staging_notary.sql
+++ models/staging_notary.sql
@@ -2,8 +2,6 @@ SELECT
     municipality_code,
     avg(sales_amount) as avg_sales_amount,
     avg(sales_price_m2) as avg_price_m2,
-    CAST(sales_date as date) as sales_new_dates
+    EXTRACT(year from CAST(sales_date as date)) as sales_dates,
 from {{ref('stg_Prello__notary')}}
-Group by municipality_code, sales_date
-
-
+Group by municipality_code, sales_dates
