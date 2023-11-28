07b91d8ec9519d573730a93d2b976bc894182f9a
Obay Al Bitar <obatsakh_bit@hotmail.com>
2023-11-28 16:01:22 +0000

    2021 notary table

--- models/intermediate/join_real_housing_notary.sql
+++ models/intermediate/join_real_housing_notary.sql
@@ -1,9 +1,4 @@
-WITH rent_sale as (SELECT *
+SELECT *
 FROM {{ref('real_estate_info_clean')}}
 JOIN {{ref('staging_notary')}} 
-USING (municipality_code))
-
-SELECT *
-FROM rent_sale
-JOIN {{ref('staging_housing_stock')}}
-USING(municipality_code)
+USING (municipality_code)
