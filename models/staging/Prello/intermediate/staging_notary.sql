84a0a8c8083e394cf8a85cf9b735ca001714e4ba
Obay Al Bitar <obatsakh_bit@hotmail.com>
2023-11-28 12:51:45 +0000

    staging Housing stock

--- models/staging/Prello/intermediate/staging_notary.sql
+++ models/staging/Prello/intermediate/staging_notary.sql
@@ -1,7 +1,7 @@
 SELECT 
     municipality_code,
-    avg(sales_amount) as avg_sales_amount,
-    avg(sales_price_m2) as avg_price_m2,
     EXTRACT(year from CAST(sales_date as date)) as sales_dates,
+    avg(sales_amount) as avg_sales_amount,
+    avg(sales_price_m2) as avg_price_m2
 from {{ref('stg_Prello__notary')}}
 Group by municipality_code, sales_dates
