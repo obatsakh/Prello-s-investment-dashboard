debe7eb5fa65b5eac2b16f2b513db86f0cf41220
Obay Al Bitar <obatsakh_bit@hotmail.com>
2023-11-28 15:28:18 +0000

    changes to tables (cleaning + joining tables)

--- models/staging/Prello/intermediate/staging_housing_stock.sql
+++ models/staging/Prello/intermediate/staging_housing_stock.sql
@@ -1,10 +1,7 @@
 SELECT 
-  municipality_code, 
-  year, 
+  municipality_code,  
   sum(nb_principal_home) as total_nb_principal_home, 
   sum(nb_second_home) as total_nb_second_home, 
   sum(nb_vacants_housing) as total_nb_vanats_housing
 FROM {{ref('stg_Prello__housing_stock')}}
-WHERE year BETWEEN 2016 AND 2021 
-GROUP BY municipality_code, year 
-ORDER BY year asc
+GROUP BY municipality_code
--- models/staging/Prello/intermediate/staging_notary.sql
+++ models/staging/Prello/intermediate/staging_notary.sql
@@ -1,7 +1,6 @@
 SELECT 
     municipality_code,
-    EXTRACT(year from CAST(sales_date as date)) as sales_dates,
     ROUND(avg(sales_amount),2) as avg_sales_amount,
     ROUND(avg(sales_price_m2),2) as avg_price_m2
 from {{ref('stg_Prello__notary')}}
-Group by municipality_code, sales_dates
+Group by municipality_code
