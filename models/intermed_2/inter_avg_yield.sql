6775f6a8b2a75139bdd3b60223f1297c63c84dec
Obay Al Bitar <obatsakh_bit@hotmail.com>
2023-11-29 08:59:29 +0000

    inter_avg_yield

--- models/intermed_2/inter_avg_yield.sql
+++ models/intermed_2/inter_avg_yield.sql
@@ -1,3 +1,3 @@
-select safe_divide(avg_price_m2, rental_med_all) as avg_yield, municipality_code, avg_price_m2
+select safe_divide(avg_price_m2, rental_med_all) as avg_yield, *
 from {{ref('join_real_estate_notary')}}
 order by avg_price_m2, avg_yield asc
