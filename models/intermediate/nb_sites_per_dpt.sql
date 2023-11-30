98de2c1f3b8141407f31237c222844753783ad21
mathieufajollesapple@gmail.com <mathieufajollesapple@gmail.com>
2023-11-30 13:04:56 +0000

    update nb_sites_per_dpt

--- models/intermediate/nb_sites_per_dpt.sql
+++ models/intermediate/nb_sites_per_dpt.sql
@@ -1,6 +1,6 @@
 select
 department_name,
-sum (nb_sites) nb_sites,
+sum (nb_sites) as nb_sites_dpt,
 count (municipality_code) as nb_municipality
 from {{ ref('nb_poi_transformed') }}
 group by department_name
