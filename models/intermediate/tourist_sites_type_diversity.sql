e61734446335291950ea08714284a8c0c6f405d8
mathieufajollesapple@gmail.com <mathieufajollesapple@gmail.com>
2023-11-30 10:51:45 +0000

    created the tourist site diversity rate tab in order to show how each department have their touristic sites divided  per category

---
+++ models/intermediate/tourist_sites_type_diversity.sql
@@ -0,0 +1,27 @@
+with poi_sites_types as ( 
+select 
+geo.department_name,
+sites.poi_type,
+count(sites.poi_type) as nb_per_type
+from {{ ref('poi_sites_types') }} as sites
+join {{ ref('geo_loc_clean') }} as geo 
+using (municipality_code)
+group by department_name, poi_type),
+
+ total_poi_type as 
+(
+    select
+        department_name,
+        sum(nb_per_type) as total_poi_type_department
+    from poi_sites_types
+     group by department_name
+)
+
+select 
+p.department_name,
+p.poi_type,
+ROUND (SAFE_DIVIDE(p.nb_per_type,t.total_poi_type_department),2) as poi_type_rate
+FROM poi_sites_types p
+left join total_poi_type t
+using(department_name)
+order by p.department_name
